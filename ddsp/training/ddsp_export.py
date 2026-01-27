9df6083d6c1629f5cc8fcaa2d8e6803e77014aab
Ashu Desai <ashud@google.com>
2026-01-26 17:21:06 -0800

    returning to import just tflite metadata

--- ddsp/training/ddsp_export.py
+++ ddsp/training/ddsp_export.py
@@ -51,10 +51,15 @@ try:
 except ImportError:
   converter = None
 
-# pylint: disable=pointless-string-statement
-
-from tflite_support import metadata as _metadata
-# pylint: enable=pointless-string-statement
+# Import only the metadata submodule to avoid binary conflicts in task/vision
+import importlib
+try:
+  _metadata = importlib.import_module('tflite_support.metadata')
+  print('Successfully imported tflite_support.metadata')
+  logging.info('Successfully imported tflite_support.metadata')
+except Exception as e:
+  print(f'Warning: Could not import tflite_support.metadata: {e}')
+  _metadata = None
 
 flags.DEFINE_string(
     'name', '', 'Name of your model to use as folder and filename on export. '
